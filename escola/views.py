947e168b5f3f242942f6fec04d0ec7d92be3e0da
João Pedro <114701807+joaopgmiranda@users.noreply.github.com>
2025-09-17 10:51:12 -0300

    initial commit

---
+++ escola/views.py
@@ -0,0 +1,12 @@
+from escola.models import Estudante, Curso
+from escola.serializers import EstudanteSerializer, CursoSerializer
+from rest_framework import viewsets
+
+class EstudanteViewSet(viewsets.ModelViewSet):
+    queryset = Estudante.objects.all()
+    serializer_class = EstudanteSerializer
+
+class CursoViewSet(viewsets.ModelViewSet):
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
+    
